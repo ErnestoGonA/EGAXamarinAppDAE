8b0ef7687559641907196fb2da6297773ca31c1d
ernesto gonzalez arellano <ergonzalezar@ittepic.edu.mx>
2018-11-09 02:22:29 -0700

    FEAT: Show all the columns in the view page

--- EGAXamarinAppDAE/EGAXamarinAppDAE/ViewModels/FicVmCatEdificiosView.cs
+++ EGAXamarinAppDAE/EGAXamarinAppDAE/ViewModels/FicVmCatEdificiosView.cs
@@ -21,7 +21,7 @@ namespace EGAXamarinAppDAE.ViewModels
         #region VARIABLES LOCALES
         private IFicSrvNavigationInventario IFicSrvNavigationInventario;
 
-        private string _LabelAlias, _LabelDes, _LabelClave;
+        private string _LabelAlias, _LabelDes, _LabelClave,_LabelFechaReg,_LabelFechaMod, _LabelUsuarioReg, _LabelUsuarioMod, _LabelActivo, _LabelBorrado;
         private short _LabelId, _LabelPrioridad;
 
         private ICommand _FicMetRegesarCatEdificiosListICommand;
@@ -99,6 +99,84 @@ namespace EGAXamarinAppDAE.ViewModels
             }
         }
 
+        public string LabelFechaReg
+        {
+            get { return _LabelFechaReg; }
+            set
+            {
+                if (value != null)
+                {
+                    _LabelFechaReg = value;
+                    RaisePropertyChanged("LabelFechaReg");
+                }
+            }
+        }
+
+        public string LabelFechaMod
+        {
+            get { return _LabelFechaMod; }
+            set
+            {
+                if (value != null)
+                {
+                    _LabelFechaMod = value;
+                    RaisePropertyChanged("LabelFechaMod");
+                }
+            }
+        }
+
+        public string LabelUsuarioReg
+        {
+            get { return _LabelUsuarioReg; }
+            set
+            {
+                if (value != null)
+                {
+                    _LabelUsuarioReg = value;
+                    RaisePropertyChanged("LabelUsuarioReg");
+                }
+            }
+        }
+
+        public string LabelUsuarioMod
+        {
+            get { return _LabelUsuarioMod; }
+            set
+            {
+                if (value != null)
+                {
+                    _LabelUsuarioMod = value;
+                    RaisePropertyChanged("LabelUsuarioMod");
+                }
+            }
+        }
+
+        public string LabelActivo
+        {
+            get { return _LabelActivo; }
+            set
+            {
+                if (value != null)
+                {
+                    _LabelActivo = value;
+                    RaisePropertyChanged("LabelActivo");
+                }
+            }
+        }
+
+        public string LabelBorrado
+        {
+            get { return _LabelBorrado; }
+            set
+            {
+                if (value != null)
+                {
+                    _LabelBorrado = value;
+                    RaisePropertyChanged("LabelBorrado");
+                }
+            }
+        }
+
         #endregion
 
         public async void OnAppearing()
@@ -110,12 +188,24 @@ namespace EGAXamarinAppDAE.ViewModels
             _LabelDes = source_eva_cat_edificios.DesEdificio;
             _LabelPrioridad = source_eva_cat_edificios.Prioridad;
             _LabelClave = source_eva_cat_edificios.Clave;
+            _LabelFechaReg = source_eva_cat_edificios.FechaReg.ToString();
+            _LabelFechaMod = source_eva_cat_edificios.FechaUltMod.ToString();
+            _LabelUsuarioReg = source_eva_cat_edificios.UsuarioReg;
+            _LabelUsuarioMod = source_eva_cat_edificios.UsuarioMod;
+            _LabelActivo = source_eva_cat_edificios.Activo;
+            _LabelBorrado = source_eva_cat_edificios.Borrado;
 
             RaisePropertyChanged("LabelId");
             RaisePropertyChanged("LabelAlias");
             RaisePropertyChanged("LabelDes");
             RaisePropertyChanged("LabelPrioridad");
             RaisePropertyChanged("LabelClave");
+            RaisePropertyChanged("LabelFechaReg");
+            RaisePropertyChanged("LabelFechaMod");
+            RaisePropertyChanged("LabelUsuarioReg");
+            RaisePropertyChanged("LabelUsuarioMod");
+            RaisePropertyChanged("LabelActivo");
+            RaisePropertyChanged("LabelBorrado");
 
         }
 
